534213733ee5c07b0ae8b7965c7d69fbc8bbee78
Fuhrmann <fuhrmanns@gmail.com>
2014-02-18 11:33:49 -0400

    Update to version 1.1.1

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 # [UcaseFirst](http://xenforo.com/community/resources/ucasefirst.84)
-==========
 
-This is an addon for the forum software XenForo. Adjust the thread title *automatically* only in the *first* letter of the thread title.
+This is an addon for the forum software XenForo. Adjust the thread title *automatically* only in the *first* letter of the thread title when creating a new thread.
 
+## 1.1.1
+- Make use of event hints in code event listeners
 
 ## 1.1.0
 - Fixed an error when trying to create a thread without title
-
-
 
 ## 1.0.0
 - Released
--- upload/library/UcaseFirst/Listener.php
+++ upload/library/UcaseFirst/Listener.php
@@ -1,19 +1,13 @@
 <?php
 class UcaseFirst_Listener
 {
-	public static function templatePostRender($templateName, &$content, array &$containerData, XenForo_Template_Abstract $template)
-	{
-		if ($templateName == 'option_template_adjustTitleCase')
-		{
-			$content = $template->create('ucasefirst_option_template_adjustTitleCase', $template->getParams())->__toString();
-		}
-	}
-	
-	public static function extend($class, array &$extend)
-	{
-		if ($class == 'XenForo_DataWriter_Discussion_Thread')
-		{
-			$extend[] = 'UcaseFirst_Extend_DataWriter_Discussion';			
-		}
-	}
+    public static function templatePostRenderAdjustTitleCase($templateName, &$content, array &$containerData, XenForo_Template_Abstract $template)
+    {
+        $content = $template->create('ucasefirst_option_template_adjustTitleCase', $template->getParams())->__toString();
+    }
+
+    public static function extendDataWriterDiscussion($class, array &$extend)
+    {
+        $extend[] = 'UcaseFirst_Extend_DataWriter_Discussion';
+    }
 }
